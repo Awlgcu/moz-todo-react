7df3351fad4dd7dcf819b1f5c4ba0b73bd6cca03
luowenbo <luowenbo@jiuaoedu.com>
2025-05-23 16:13:10 +0800

    待完成更新待办

--- src/App.js
+++ src/App.js
@@ -5,22 +5,56 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 
 export default function App(props) {
+  //将传入的任务参数定义为变量，再将变量转换成任务对应的Todo组件
   const [tasks, setTasks] = useState(props.tasks);
+  function toggleTaskCompleted(id) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+  //创建一个删除任务的函数，与添加任务函数相同的处理
+  function deleteTask(id) {
+    const newTasks = tasks.filter((task) => task.id != id);
+    setTasks(newTasks);
+  }
+  //创建一个编辑任务的函数
+  function editTask(id, newName) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, name: newName };
+      } else {
+        return task;
+      }
+    });
+    setTasks(newTasks);
+  }
 
-  const taskList = tasks.map((task) => (
-    <Todo
-      taskName={task.name}
-      id={task.id}
-      completed={task.completed}
-      key={task.id}
-    />
-  ));
+  const taskList = tasks.map((task) => {
+    return (
+      <Todo
+        taskName={task.name}
+        id={task.id}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    );
+  });
 
-  //创建一个添加任务的函数，并将其作为属性传递给Form组件，这样Form组件在提交时就能调用这个函数修改App组件中的tasks变量从而增加一个<Todo>组件？？？
+  //创建一个添加任务的函数，并将其作为属性传递给Form组件，这样Form组件在提交时就能调用这个函数修改App组件中的tasks变量从而增加一个<Todo>组件
   function addTask(name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false };
     setTasks([...tasks, newTask]);
   }
+
+  const taskNoun = taskList.length == 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${taskNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -31,7 +65,7 @@ export default function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,16 +4,29 @@ export default function Todo(props) {
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleTaskCompleted(props.id)}
+        />
         <label className="todo-label" htmlFor={props.id}>
           {props.taskName}
         </label>
       </div>
       <div className="btn-group">
-        <button type="button" className="btn">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => props.editTask(props.id, "a")}
+        >
           Edit <span className="visually-hidden">{props.taskName}</span>
         </button>
-        <button type="button" className="btn btn__danger">
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteTask(props.id)}
+        >
           Delete <span className="visually-hidden">{props.taskName}</span>
         </button>
       </div>

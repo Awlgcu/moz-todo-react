2a5608036164b3ee5a362e6699862c779344c1c2
luowenbo <luowenbo@jiuaoedu.com>
2025-05-23 17:40:55 +0800

    feat:新增了编辑和筛选功能

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import FilterButton from "./components/FilterButton";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 export default function App(props) {
   //将传入的任务参数定义为变量，再将变量转换成任务对应的Todo组件
   const [tasks, setTasks] = useState(props.tasks);
+  //定义筛选过滤器变量
+  const [filter, setFilter] = useState("All");
+
   function toggleTaskCompleted(id) {
     const newTasks = tasks.map((task) => {
       if (task.id === id) {
@@ -33,10 +43,28 @@ export default function App(props) {
     setTasks(newTasks);
   }
 
-  const taskList = tasks.map((task) => {
+  //创建一个处理过滤筛选的函数
+  function filterTask(fliterName) {
+    const newTasks = tasks.filter(FILTER_MAP[fliterName]);
+    setTasks(newTasks);
+  }
+
+  const filterList = FILTER_NAMES.map((name) => {
+    return (
+      <FilterButton
+        key={name}
+        name={name}
+        isPressed={name === filter}
+        setFilter={setFilter}
+      />
+    );
+  });
+
+  //filter只能在tasks转化为<Todo>组件时来做筛选，不能直接用于过滤tasks生成newTasks，这样会丢失原本tasks，出现错误
+  const taskList = tasks.filter(FILTER_MAP[filter]).map((task) => {
     return (
       <Todo
-        taskName={task.name}
+        name={task.name}
         id={task.id}
         completed={task.completed}
         key={task.id}
@@ -46,6 +74,23 @@ export default function App(props) {
       />
     );
   });
+
+  //下述代码与上述代码具有相同效果
+  // const taskList = tasks.map((task) => {
+  //   if (FILTER_MAP[filter](task)) {
+  //     return (
+  //       <Todo
+  //         name={task.name}
+  //         id={task.id}
+  //         completed={task.completed}
+  //         key={task.id}
+  //         toggleTaskCompleted={toggleTaskCompleted}
+  //         deleteTask={deleteTask}
+  //         editTask={editTask}
+  //       />
+  //     );
+  //   }
+  // });
 
   //创建一个添加任务的函数，并将其作为属性传递给Form组件，这样Form组件在提交时就能调用这个函数修改App组件中的tasks变量从而增加一个<Todo>组件
   function addTask(name) {
@@ -60,11 +105,7 @@ export default function App(props) {
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form onSubmit={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
+      <div className="filters btn-group stack-exception">{filterList}</div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
         className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,49 @@
-import React from "react";
+import { useState } from "react";
 
 export default function Todo(props) {
-  return (
-    <li className="todo stack-small">
+  const [isEditing, setEditing] = useState(false);
+  const [newName, setNewName] = useState(props.name);
+
+  function saveNewName(event) {
+    setNewName(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.editTask(props.id, newName);
+    setNewName("");
+    setEditing(false);
+  }
+  const editingTemplate = (
+    <form className="stack-small" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="todo-label" htmlFor={props.id}>
+          New name for {props.name}
+        </label>
+        <input
+          id={props.id}
+          className="todo-text"
+          type="text"
+          onChange={saveNewName}
+        />
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn todo-cancel"
+          onClick={() => setEditing(false)}
+        >
+          Cancel
+          <span className="visually-hidden">renaming {props.name}</span>
+        </button>
+        <button type="submit" className="btn btn__primary todo-edit">
+          Save
+          <span className="visually-hidden">new name for {props.name}</span>
+        </button>
+      </div>
+    </form>
+  );
+  const viewTemplate = (
+    <div className="stack-small">
       <div className="c-cb">
         <input
           id={props.id}
@@ -11,25 +52,22 @@ export default function Todo(props) {
           onChange={() => props.toggleTaskCompleted(props.id)}
         />
         <label className="todo-label" htmlFor={props.id}>
-          {props.taskName}
+          {props.name}
         </label>
       </div>
       <div className="btn-group">
-        <button
-          type="button"
-          className="btn"
-          onClick={() => props.editTask(props.id, "a")}
-        >
-          Edit <span className="visually-hidden">{props.taskName}</span>
+        <button type="button" className="btn" onClick={() => setEditing(true)}>
+          Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button
           type="button"
           className="btn btn__danger"
           onClick={() => props.deleteTask(props.id)}
         >
-          Delete <span className="visually-hidden">{props.taskName}</span>
+          Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
-    </li>
+    </div>
   );
+  return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 }
